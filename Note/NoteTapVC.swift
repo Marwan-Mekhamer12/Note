217132742792fe8506551acb1c940469a28df905
Marwan Mekhamer <Allosaurus1212@gmail.com>
2025-02-09 17:07:45 -0800

    first commit

---
+++ Note/NoteTapVC.swift
@@ -0,0 +1,29 @@
+//
+//  NoteTapVC.swift
+//  Note
+//
+//  Created by Marwan Mekhamer on 08/08/2024.
+//
+
+import UIKit
+
+class NoteTapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
